3a3a5f504b8de0c72cf736f3daf2014a10911b10
thomas <victoria.thomas-pro@hotmail.fr>
2016-12-13 14:06:44 +0100

    fix(.idea): delete .idea/

--- app/src/main/java/com/serious/budgeat/Activity/MainActivity.java
+++ app/src/main/java/com/serious/budgeat/Activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.serious.budgeat;
+package com.serious.budgeat.Activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.serious.budgeat.R;
 
 public class MainActivity extends AppCompatActivity {
 
