974232d82a815f31a3b6b587d59b783be96a3e18
thomas <victoria.thomas-pro@hotmail.fr>
2016-12-13 15:23:29 +0100

    feat(connexion): landing screen

--- app/src/main/java/com/serious/budgeat/Activity/InscriptionActivity.java
+++ app/src/main/java/com/serious/budgeat/Activity/InscriptionActivity.java
@@ -2,24 +2,14 @@ package com.serious.budgeat.Activity.activities.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.TextView;
 
 import com.serious.budgeat.R;
 
-import butterknife.BindString;
-import butterknife.BindView;
-import butterknife.ButterKnife;
-import butterknife.OnClick;
-
-public class MainActivity extends AppCompatActivity {
+public class InscriptionActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_inscription);
     }
-
-
-
 }
