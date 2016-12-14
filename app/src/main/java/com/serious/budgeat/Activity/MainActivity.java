33c9d0d79306d069317d267b98e7c4ecea3d0f09
thomas <victoria.thomas-pro@hotmail.fr>
2016-12-14 01:40:03 +0100

    feat(home): order button

--- app/src/main/java/com/serious/budgeat/Activity/MainActivity.java
+++ app/src/main/java/com/serious/budgeat/Activity/MainActivity.java
@@ -1,9 +1,13 @@
 package com.serious.budgeat.Activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.serious.budgeat.R;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -12,8 +16,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
 
+    @OnClick(R.id.orderPageButton)
+    void gotoOrder(){
+        Intent intent = new Intent(MainActivity.this, OrderActivity.class);
+        startActivity(intent);
+    }
 
 
 }
